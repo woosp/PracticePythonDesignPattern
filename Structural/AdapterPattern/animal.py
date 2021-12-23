662bb70294ce3600526c82070efc117847d1e313
Peter <woosp2@gmail.com>
2021-12-22 22:53:04 -0800

    Add a file for adapter pattern

--- Structural/AdapterPattern/animal.py
+++ Structural/AdapterPattern/animal.py
@@ -28,10 +28,26 @@ makeWalk(bingo)
 # makeWalk(nemo)
 
 
+# from youtube
 class AnimalFishAdapter(Animal): 
-    def __init__(self, fish: Fish): 
+    def __init__(self, fish: Fish):    
         self.fish = fish
     def walk(self):
         self.fish.swim()
 
+# Me trying differnet way
+# class AnimalFishAdapter(Animal): 
+#     def __init__(self, animal: Animal):
+#         self.animal = animal
+#     def walk(self):
+#         if isinstance(self.animal , Fish):
+#             self.animal.swim()
+#         elif isinstance(self.animal, Dog):
+#             self.animal.walk()
+#         else:
+            # print("invalid instance!!")
+            
+print("----")
 makeWalk(AnimalFishAdapter(nemo))
+# makeWalk(AnimalFishAdapter(bingo))
+# makeWalk(AnimalFishAdapter(kitty))
